d32a27bf829c9542cbc0eaf5a95aa23a60029b14
bambademe <cheikhbambademe@gmail.com>
2022-03-12 08:34:26 -0800

    project setup

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+const options = {
+  arrowParens: "avoid",
+  signleQuote: true,
+  backetSpacing: true,
+  endOfLinf: "lf",
+  semi: false,
+  tabWidth: 2,
+  trailingComma: "none",
+};
+
+module.exports = options;
